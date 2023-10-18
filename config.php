b254d74385dd98a42cd3b334996f650d8d2c6c8b
Maxwell Jones <maxxjonesyy@gmail.com>
2023-10-19 09:34:28 +1100

    init mysql and env variables

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+try {
+    $pdo = new PDO("mysql:host=" . $_ENV['hostname'] . "dbname=" . $_ENV['database'], $_ENV['username'], $_ENV['password']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
