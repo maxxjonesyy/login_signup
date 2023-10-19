7b8aa08bbf02a8226eccebdb3be140012171f1a0
Maxwell Jones <maxxjonesyy@gmail.com>
2023-10-19 16:25:30 +1100

    add session to config

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php 
 
+session_start();
+
 require __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 try {
-    $pdo = new PDO("mysql:host=" . $_ENV['hostname'] . "dbname=" . $_ENV['database'], $_ENV['username'], $_ENV['password']);
+    $pdo = new PDO("mysql:host=" . $_ENV['hostname'] . ";dbname=" . $_ENV['database'], $_ENV['username'], $_ENV['password']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
